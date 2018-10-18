e1bdb2e1b956512d8edb2cc1ab27d909888a1031
lakme <lakmariappan1997@gmail.com>
2018-10-18 16:50:39 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import SearchPage from './SearchPage';
+import MainPage from './MainPage';
+import * as BooksAPI from './BooksAPI';
+import { Route } from 'react-router-dom';
+import autobind from 'autobind-decorator'
+import './App.css'
+
+class BooksApp extends React.Component {
+  state = {
+    books: []
+  }
+  componentDidMount(){
+    BooksAPI.getAll().then((books) => {
+      this.setState({books: books})
+    })
+    
+  }
+
+  
+  moveShelf = (book, shelf) => 
+   {
+    BooksAPI.update(book, shelf);
+
+    BooksAPI.getAll().then((books) => {
+      this.setState({books: books})
+    })
+    
+    }
+
+  
+  render() {
+    return (
+      <div className="app">
+      <Route exact path = "/" render={() =>(
+        <MainPage
+      books={this.state.books}
+      moveShelf={this.moveShelf}
+      />
+      )} />
+      
+      <Route path = "/search" render={() =>(
+      <SearchPage
+      moveShelf={this.moveShelf}
+      books={this.state.books}
+      />
+      )} />
+      </div>
+    )
+  }
+}
+
+export default BooksApp
